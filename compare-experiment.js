769f515102f7c0c867d65440dd4d40740161a298
Jenny Newton <jennynewton@live.co.uk>
2022-04-04 21:54:13 +0100

    Refactor findLetterInCorrectPlace function for easier testing. Fix issue with logging matches in guessArray.

--- compare-experiment.js
+++ compare-experiment.js
@@ -1,32 +1,32 @@
-let todaysWord = "cat";
-let todaysWordArray = todaysWord.split("");
-
-let guess = "tab";
-let guessArray = guess.split("");
-
-let result = [".", ".", "."];
-
-// Can I 'DRY' the above? Array thing is repetitive. I will think on this once I capture the input for "guess".
 
 function findLetterInCorrectPlace(guess, word) {
-    
+
+    let todaysWordArray = word.split("");
+    let guessArray = guess.split("");
+    let result = [".", ".", "."];
+
+    console.log(`Today's word: ${todaysWordArray}`);
+
     // Check if item at same index of each array is same. If it is, place it in 'result' & remove matching item from todaysWordArray. Else, add X to result. 
+
     for (let i = 0; i < guess.length; i++) {
         if(guess[i] === word[i]) {
             result[i] = word[i];
-            todaysWordArray.splice(i, 1);
+            todaysWordArray[i] = " ";
         }
         else {
-            result[i] = "X";
+            result[i] = "*";
         }
     }
 
+    console.log(`Guess: ${guessArray}`);
+    console.log(`Today's word with matches removed: ${todaysWordArray}`);
+    console.log(`Result: ${result}`);
+
 }
 
-// Identify correct letters in correct place, remove from todaysWord array
-// Identify correct letters in wrong place, replace with a non-alphabet symbol so as to not mess up the array (still to do this)
+findLetterInCorrectPlace("cat", "bat");
 
-findLetterInCorrectPlace(guessArray, todaysWordArray);
-console.log(`Guess: ${guessArray}`);
-console.log(`Today's word with matches removed: ${todaysWordArray}`);
-console.log(`Result: ${result}`);
+findLetterInCorrectPlace("bird", "bead");
+
+findLetterInCorrectPlace("tea", "tea");
