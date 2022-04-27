243ddcb9551c5bd4e8c14f32c1120403e54264f1
Jenny Newton <jennynewton@live.co.uk>
2022-04-27 22:12:37 +0100

    Create element, turn array to string and add as HTML, append element - in prep for returning result of Wordle guess

--- wordle.js
+++ wordle.js
@@ -1,13 +1,16 @@
 
 // Testing logging and changing input value without jQuery - new learnings!
 
-let formInput = document.querySelector("input").value;
-console.log(formInput); // should log "Hello"
+let guessInput = document.querySelector("input").value;
+console.log(guessInput); // should log "Hello"
 
-document.querySelector("input").value = "Goodbye";
-console.log(document.querySelector("input").value); // should log "Goodbye"
-console.log(formInput); // should STILL log "Hello"
-
+let testArray = ["<span class=\"red\">A</span>", "<span class=\"blue\">B</span>", "<span class=\"green\">C</span>", "", ""];
+let paragraph = document.createElement('p');
+let testHtmlString = testArray.join('');
+paragraph.innerHTML = testHtmlString;
+let resultDiv = document.querySelector("div#result");
+console.log(resultDiv);
+resultDiv.append(paragraph);
 
 // Need to refactor matchLetters to return guess with correct / incorrect markers. 
 
@@ -46,3 +49,12 @@ function matchLetters(guess, word) {
 // matchLetters("tat", "tot");
 // matchLetters("mum", "umm");
 // matchLetters("add", "day");
+
+
+/* Instructions to self! 
+- Take input value, send it into function. 
+- function needs to push items into array (problems - how to add the letter plus the span HTML?)
+- eventually, will need to run the function on click
+- scope! Where do all the variables need to be?
+- multiple guesses - that's going to make things complicated... Need to eventually rebuild in React or something.
+*/
