a6ed1dd86a4be979c2b1a0cbaf538112a4e96d19
Jenny Newton <jennynewton@live.co.uk>
2022-05-26 22:30:04 +0100

    Refactor matchLetters function to make it work with new interface

--- wordle.js
+++ wordle.js
@@ -3,36 +3,35 @@
 
     let words = ["daisy", "henge", "roots", "bathe", "shoes", "fluff", "dirty", "clean", "roads", "kitty", "tiger", "grass", "bench", "range", "acres", "blank", "walls", "viola", "words", "hives", "hover", "mouse", "honey", "scarf", "trees", "hares", "books", "tease", "zebra", "lolly"];
     
-    // Why does this array only seem to receive the first 'change' - for example, on first guess you get 'A' correct but in incorrect place, A goes yellow. If you get it in the correct place on guess 2, it doesn't go green. WHYYYY??? Console seems to log the new version at that position but in the actual array it's not there. Grrr.
-    // OF COURSE!!!! The function can't find the index of 'A' in alphabet cos it's been changed to <span> etc. So it doesn't add the new colour. 
-    // Maybe I need 2 arrays, one immutable alphabet string to find the index of the guess letter, and the existing alphabet array to actually be changed. 
     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const alphabetIndex = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     let chosenWord = words[Math.floor(Math.random() * words.length)];
     let guessNumber = 0;
 
-    // let alphabetString = alphabet.join('');
+    let alphabetString = alphabet.join('');
     let alphabetDiv = document.querySelector("#alphabet");
-    alphabetDiv.innerHTML = alphabetIndex;
+    alphabetDiv.innerHTML = alphabetString;
 
-    // let button = document.querySelector("button"); // removing button stuff
     let inputField = document.querySelector(".textfield");
-    // button.addEventListener('click', () => matchLetters(chosenWord));  // removing button stuff
+
     inputField.addEventListener('keyup', (e) => {
    
-        updateDisplay(inputField.value, e);
+        updateDisplay(inputField.value, e, chosenWord);
 
-        if (e.key === 'Enter') {
-            matchLetters(chosenWord);
-        }
-        
     });
 
-    function updateDisplay(guessInput, e) {
+    function updateDisplay(guess, e, word) {
 
         let displayLetters = document.querySelectorAll("letter-input");
-        let guessArray = guessInput.toUpperCase().split("");
+        let guessArray = guess.toUpperCase().split("");  // Do I really need to split this into an array in this function? Could just turn the word uppercase
+
+        if (e.key === 'Enter' && guess.length === 5) {
+            if (e.key === 'Enter') {
+                matchLetters(guess, word);
+            }
+        }
+
         displayLetters.forEach(function(box, index) {
             if (guessArray[index]) {
                 box.innerHTML = guessArray[index];
@@ -41,18 +40,12 @@
                 box.innerHTML = " ";
             }
         });
-        guessArray.forEach(function(letter, index) {
-            displayLetters[index].innerHTML = letter;
-        });
-        console.log(guessInput);
         
     }
 
-    function matchLetters(word) {
+    function matchLetters(guess, word) {
 
         console.log(word);
-
-        // Refactor the below to work with new interface - is broken now! :D 
         
         if (guessNumber >= 6) {
             inputField.setAttribute("readonly", "");
@@ -62,13 +55,10 @@
 
         else {
 
-            // guessInput needs to feed live into the 'display' boxes. 
-            // Moving this line into the updateDisplay function: let guessInput = document.querySelector(".textfield").value;
-            let resultArray = ["", "", "", "", ""];
-            // Moving this line to updateDisplay function: let guessArray = guessInput.toUpperCase().split("");
+            let resultArray = ["", "", "", "", ""]; 
+            let guessArray = guess.toUpperCase().split("");
             let wordArray = word.toUpperCase().split("");
 
-            // Refactor function to use forEach, as practice. Is this harder to read? Less obvious what the function does cos isn't named...
             guessArray.forEach(function(letter, index, guess) {
                     if (letter === wordArray[index]) {
                         resultArray[index] = `<span class=\"correct\">${letter}</span>`;
